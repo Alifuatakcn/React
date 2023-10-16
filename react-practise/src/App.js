6d8834d891a94c320debbbb47fbf9e8a056249d6
Alifuatakcn <fakcan4908@gmail.com>
2023-10-16 19:36:43 +0300

    practise2

--- react-practise/src/App.js
+++ react-practise/src/App.js
@@ -1,10 +1,14 @@
+// eslint-disable-next-line
 import Dersler from "./components/component1/dersler";
-
+import Practice2 from "./components/component2/practice2";
 
 function App() {
   return (
-       <Dersler/>
-       );
+    <>
+      {/* <Dersler /> */}
+      <Practice2 />
+    </>
+  );
 }
 
 export default App;
--- react-practise/src/components/component1/dersler.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-const Dersler= () =>{
-    return (
-     
-    <div>React Practise</div>
-    )
-};
-export default Dersler;
