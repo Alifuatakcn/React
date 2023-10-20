e7f990d2ea424ac14264c39b61abf02e9d8b2c3e
Alifuatakcn <fakcan4908@gmail.com>
2023-10-21 00:28:21 +0300

    useEffect-practise

--- myapp/src/components/18-country-filter/FilteredCountries.jsx
+++ myapp/src/components/18-country-filter/FilteredCountries.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
-import countries from "../../assets/data.json"
+import countries from "../../assets/data/countries.json"
+import {Table} from "react-bootstrap"
+
 
 const FilteredCountries = () => {
   return (
-    <div>FilteredCountries</div>
+    <div>
+<Table striped bordered hover variant='dark'
+
+style={{textAlign:"center",cursor:"pointer",width:"50rem",margin:"3rem auto"}}
+>
+    <thead>
+        <tr>
+            <th>Ulke No</th>
+            <th>Ulke Adi</th>
+            <th>Ulke Kodu</th>
+        </tr>
+    </thead>
+<tbody>
+    {
+         countries.map((item,index)=>(
+
+            <tr key={item.code}>
+                <td>{index+1}</td>
+                <td>{item.name}</td>
+                <td>{item.code}</td>
+              
+
+
+            </tr>
+         ))
+
+    }
+
+</tbody>
+
+</Table>
+
+    </div>
   )
 }
 
