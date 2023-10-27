1abfa59493a9463a40e0c9e7f1779729528e900e
Alifuatakcn <fakcan4908@gmail.com>
2023-10-27 22:51:12 +0300

    practise

--- myapp/src/components/27-context-api/exchange.jsx
+++ myapp/src/components/27-context-api/exchange.jsx
@@ -1,21 +1,39 @@
 import React, { useContext } from "react";
 import StoreContext from "../../store";
-import { Button, ButtonGroup, InputGroup } from "react-bootstrap";
-import { Form } from "react-router-dom";
+import { Form, InputGroup } from "react-bootstrap";
 
 const Exchange = () => {
   const store = useContext(StoreContext);
+  const { currencies } = store;
+
+  const handleChange = (e) => { 
+    console.log(e.target.value)
+   }
 
   return (
     <div>
-    <InputGroup className="mb-3">
-        <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+      <InputGroup className="mb-3">
         <Form.Control
-    
-          placeholder="Username"
-          aria-label="Username"
+          placeholder="Type amount"
+          aria-label="Type amount"
           aria-describedby="basic-addon1"
         />
+
+        <Form.Select
+          aria-label="Default select example"
+          onChange={handleChange} value=""
+        >
+          <option disabled>
+            Currency
+          </option>
+          {Object.keys(currencies).map((item) => (
+            <option key={item} value={item}>
+              {item}
+            </option>
+          ))}
+        </Form.Select>
+
+        <InputGroup.Text id="basic-addon1">Result:</InputGroup.Text>
       </InputGroup>
     </div>
   );
