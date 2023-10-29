33a7494ee795a68955509e0623fbc05001c741f6
Alifuatakcn <fakcan4908@gmail.com>
2023-10-29 19:24:47 +0300

    son version

--- myapp/src 2/components/06-props-2-children/Uruncards.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Uruncards = (props) => {
-  return (
-    <div className='urun-cards'>{props.children}</div>
-  )
-}
-
-export default Uruncards
--- myapp/src 2/store/index.js
+++
@@ -1,7 +0,0 @@
-import{ createContext } from "react";
-
-// Boş bir merkezi state oluşturur.
-const StoreContext = createContext();
-
-// Merkezi state import edilmeye hazır hale getirildi
-export default StoreContext;
